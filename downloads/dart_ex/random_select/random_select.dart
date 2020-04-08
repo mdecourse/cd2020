a8c04c05d1d542a46b23b5e41e2135d6adc9e345
mdecourse <mdecourse@gmail.com>
2020-04-08 17:53:02 +0800

    add random select program

--- downloads/dart_ex/random_select/random_select.dart
+++ downloads/dart_ex/random_select/random_select.dart
@@ -1,54 +1,43 @@
 import 'dart:html';
 import 'dart:convert';
 
-  InputElement studListUrl = querySelector("#studListUrl");
-  String studUrl;
-  InputElement number = querySelector("#number");
-  int num;
-  // 將 Label 改為 Textarea, 避免產生過程結果嵌入所在頁面
-  TextAreaElement output = querySelector("#output");
- 
+SelectElement studListUrl = querySelector("#studListUrl");
+String grpList;
+InputElement numOfStud = querySelector("#numOfStud");
+int num;
+// 將 Label 改為 Textarea, 避免產生過程結果嵌入所在頁面
+TextAreaElement output = querySelector("#output");
+
 main() {
   querySelector("#submit").onClick.listen((e) => randomSelect());
 }
 
-
-//String grpList = 'https://mde.tw/cd2020/downloads/2a_group_list.txt';
-//int num = 2;
-
 randomSelect() {
-
   output.innerHtml = "";
- 
+
   if (studListUrl.value != "") {
-    studUrl = studListUrl.value;
+    grpList = studListUrl.value;
   } else {
-    studUrl = 'https://mde.tw/cd2020/downloads/2a_group_list.txt';
+    grpList = 'https://mde.tw/cd2020/downloads/2a_group_list.txt';
   }
-  
-  if (number.value != ""){
-    num = number.value;
+
+  if (numOfStud.value != "") {
+    num = int.parse(numOfStud.value);
   } else {
-      num = 2;
+    num = 2;
   }
-  
-  HttpRequest.getString(studUrl)
-    // assign response as variable resp
-    .then((String resp){
-      // remove all "'" to allow jsonDecode string into list
-      final replaced = resp.replaceAll("'", '');
-      List studList = jsonDecode(replaced);
-      for (int i=0; i<studList.length; i++)
-      {
-        // shuffle studList element
-        studList[i].shuffle();
-        // draw num of member from each group
-        //print("-"*20);
-	output
-        for (int j=0; j<num; j++)
-        {
-          print("group ${i+1}:" + studList[i][j].toString());
-        }
+
+  HttpRequest.getString(grpList).then((String resp) {
+    final replaced = resp.replaceAll("'", '');
+    List studList = jsonDecode(replaced);
+    for (int i = 0; i < studList.length; i++) {
+      // shuffle studList element
+      studList[i].shuffle();
+      // draw num of member from each group
+      output.text += "-" * 20 + "\n";
+      for (int j = 0; j < num; j++) {
+        output.text += "group ${i + 1}:" + studList[i][j].toString() + "\n";
       }
-    });
+    }
+  });
 }
